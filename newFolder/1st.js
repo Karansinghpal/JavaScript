3a514c6ab9049fa77a80ac414b23bf017e27bcd7
KARAN SINGH PAL <118987213+Karansinghpal@users.noreply.github.com>
2025-06-18 13:27:17 +0000

    config

---
+++ newFolder/1st.js
@@ -0,0 +1,9 @@
+console.log("Karan");
+const accountId= 765122;
+let name = ("Vishal");
+let rollNo = 45;
+let age = 22;
+console.table([ "accoundId"= accountId,"name",name, "rollNo.",rollNo,"age",age]);
+console.log("Done");
+
+
---
+++ newFolder/dataType.js
@@ -0,0 +1,27 @@
+console.log("DataTypes");
+const accountId= 765122;
+let name = ("Vishal");
+let school;
+let rollNo = 45;
+let age = 22;
+console.table([ accountId,name,rollNo,age]);
+console.log("Done");
+
+
+/*
+number
+string
+bigint
+boolean
+null = stand alone value
+undifined if value is not define 
+symbol = unique
+
+*/
+console.log(typeof accountId);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof rollNo);
+console.log(typeof school);
+console.log(typeof null);
+console.log(typeof undefined);
